ef9533e38f43181b9ff3691b7125c2e218313e2b
Technomancer <frederik.amfelt@gmail.com>
2018-03-06 14:56:52 +0100

    Initial commit

---
+++ content.js
@@ -0,0 +1,22 @@
+var checkPageButton = document.getElementById('checkPage');
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+
+            var replacedText = text.replace ( /(afmeld)/gi, "Klik her for at fjerne e-mails" );
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+
+
+            }
+        }
+    }
+}
